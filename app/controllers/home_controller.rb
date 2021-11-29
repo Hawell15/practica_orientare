193e468440172352f13cadaf29bc02c35ff60bd8
Hawell15 <roma.ciobanu@gmail.com>
2021-11-29 22:36:22 +0200

    add devise add competition from file

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,17 @@
 class HomeController < ApplicationController
+  include HomeHelper
+
   def index
     @clubs_count        = Club.count
     @runners_count      = Runner.count
     @competitions_count = Competition.count
     @results_count      = Result.count
+  end
+
+  def add_competition_file
+    return unless params[:path]
+
+    path = params[:path].tempfile.path
+    parse_data(path)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_19_111311) do
+ActiveRecord::Schema.define(version: 2021_11_29_151525) do
 
   create_table "categories", force: :cascade do |t|
     t.string "category_name"
@@ -74,6 +74,18 @@ ActiveRecord::Schema.define(version: 2021_11_19_111311) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["category_id"], name: "index_runners_on_category_id"
     t.index ["club_id"], name: "index_runners_on_club_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
